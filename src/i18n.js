f17053bbc968abadac408f7ecf055593381d4c75
Luis <68544338+luiso2@users.noreply.github.com>
2026-09-22 11:54:21 -0400

    Add waxing service with supplied video

--- src/i18n.js
+++ src/i18n.js
@@ -302,3 +302,5 @@ Object.assign(EN, {
 Object.assign(EN, { "media.illustrative": "Illustrative image" });
 
 Object.assign(EN, { "srv.detoxName": "Detox Facial + Hyaluronic Acid Hydration", "srv.f2": "A detox facial cleanse followed by hyaluronic acid hydration for skin that feels fresh and soft." });
+
+Object.assign(EN, { "srv.tab4": "Brows & Waxing", "srv.waxName": "Waxing", "srv.waxDesc": "Wax hair removal and skin care. Ask about available treatment areas and the right option for you.", "srv.waxPrice": "Ask for pricing" });
